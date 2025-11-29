e2787a0156f236899f60ab716c5d2249d09b0034
Paolo Paci <paolopci@yahoo.it>
2025-11-29 15:18:00 +0100

    Add RazorComponent for Banners (CRUD)

--- OnlineShopBlazor/Models/Db/OnLineShopContext.cs
+++ OnlineShopBlazor/Models/Db/OnLineShopContext.cs
@@ -6,15 +6,26 @@ namespace OnlineShopBlazor.Models.Db;
 
 public partial class OnLineShopContext : DbContext
 {
-    public OnLineShopContext() { }
+    public OnLineShopContext()
+    {
+    }
 
     public OnLineShopContext(DbContextOptions<OnLineShopContext> options)
-        : base(options) { }
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Banner> Banners { get; set; } = null!;
 
     public virtual DbSet<Menu> Menus { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Banner>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_Banner_ID");
+        });
+
         modelBuilder.Entity<Menu>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK_Menus_ID");
